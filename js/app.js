ddfbd367a2fe7aa8fd5b6353685d182344b24ccf
kropacheven <81347649+kropacheven@users.noreply.github.com>
2021-08-28 21:39:41 +0800

    correction_28082021

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 
-const search = new Filter('search', 'data-caption');
+//const search = new Filter('search', 'data-caption');
 
 baguetteBox.run('.gallery');
 
 
 // JavaScript searchbar 
+
+document.getElementById("searchbar").addEventListener("keyup", search_photo);
+
 function search_photo() {
     let input = document.getElementById('searchbar').value;
     input=input.toLowerCase();
+    console.log(input);
     let x = document.getElementsByClassName('photosearch');
+    
       
     for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        if (!x[i].getAttribute('data-caption').toLowerCase().includes(input)) {
             x[i].style.display="none";
         }
         else {
-            x[i].style.display="gallery-item";                 
+            x[i].style.display="grid";              
         }
     }
 }
